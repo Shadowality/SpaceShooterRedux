539bd98ff8263c03ea8b55f7d9db6f69d54544bc
Shadowality <vicentevieira619@gmail.com>
2017-04-11 00:15:50 +0200

    Add files via upload

---
+++ Managers/ObjectPoolerManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObjectPoolerManager : Singleton<ObjectPoolerManager>
+{
+    #region Public Fields
+
+    public List<ObjectPoolItem> objectsToPool;
+
+    public List<GameObject> pooledObjects;
+
+    #endregion Public Fields
+
+    #region Public Methods
+
+    // Get pooled object by name.
+    public GameObject GetPooledObject(string nameObject)
+    {
+        // Check if theres any pooled object active and its name.
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].name == nameObject)
+                return pooledObjects[i];
+        }
+
+        // Add object to pool if possible.
+        foreach (ObjectPoolItem item in objectsToPool)
+        {
+            if (item.objectToPool.name == nameObject && item.expandPoolAtRuntime)
+                return GetGameObject(item);
+        }
+
+        return null;
+    }
+
+    #endregion Public Methods
+
+    #region Protected Methods
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    #endregion Protected Methods
+
+    #region Private Methods
+
+    private GameObject GetGameObject(ObjectPoolItem item)
+    {
+        item.objectToPool.SetActive(false);
+        GameObject obj = Instantiate(item.objectToPool) as GameObject;
+        obj.transform.parent = item.Pool.transform;
+        obj.name = item.objectToPool.name;
+        obj.SetActive(false);
+        pooledObjects.Add(obj);
+        return obj;
+    }
+
+    private void Init()
+    {
+        GameObject pools = new GameObject("ObjectPools");
+        pooledObjects = new List<GameObject>();
+
+        foreach (ObjectPoolItem item in objectsToPool)
+        {
+            item.Pool = new GameObject(item.objectToPool.name + "_Pool");
+            item.Pool.transform.parent = pools.transform;
+
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                GetGameObject(item);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "02 Game")
+            Init();
+    }
+
+    #endregion Private Methods
+
+    #region Public Classes
+
+    [System.Serializable]
+    public class ObjectPoolItem
+    {
+        #region Public Fields
+
+        public int amountToPool;
+        public bool expandPoolAtRuntime;
+        public GameObject objectToPool;
+
+        #endregion Public Fields
+
+        #region Private Fields
+
+        private GameObject pool;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        public GameObject Pool { get; set; }
+
+        #endregion Public Properties
+    }
+
+    #endregion Public Classes
+}
---
+++ Managers/OptionsManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsManager : MonoBehaviour
+{
+
+    private static Slider musicSlider;
+    private static Slider sfxSlider;
+
+    // Use this for initialization
+    void Awake()
+    {
+        musicSlider = GameObject.Find("MusicSlider").GetComponent<Slider>();
+        sfxSlider = GameObject.Find("SFXSlider").GetComponent<Slider>();
+
+        musicSlider.value = PlayerPrefsManager.GetMusicVolume();
+        sfxSlider.value = PlayerPrefsManager.GetSFXVolume();
+    }
+
+    public void SetMusicVolume()
+    {
+        AudioManager.Instance.SetMusicVolume(musicSlider.value);
+    }
+
+    public void SetSFXVolume()
+    {
+        AudioManager.Instance.SetSFXVolume(sfxSlider.value);
+        if (!AudioManager.Instance.GetSFXAudioSource("ShieldOn").isPlaying)
+            AudioManager.Instance.GetSFXAudioSource("ShieldOn").Play();
+    }
+
+    public void SetDefaults()
+    {
+        musicSlider.value = PlayerPrefsManager.GetDefaultMusicVolume();
+        sfxSlider.value = PlayerPrefsManager.GetDefaultSFXVolume();
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsManager.SetMusicVolume(musicSlider.value);
+        PlayerPrefsManager.SetSFXVolume(sfxSlider.value);
+    }
+
+}
